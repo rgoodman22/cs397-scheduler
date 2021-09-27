a7acf7be4ebcf8d72d2910981bfbc9a452aaa63f
rgoodman22 <goodman.rob24@gmail.com>
2021-09-27 18:42:42 -0500

    read from firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CourseList from './components/CourseList';
 import './App.css';
+import { useData } from './utilities/firebase';
 
 const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
 
@@ -34,20 +35,9 @@ const Banner = ({ title }) => (
 );
 
 const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
-
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
-    }
-    fetchSchedule();
-  }, []);
-
-  if (!schedule) return <h1>Loading schedule...</h1>;
+  const [schedule, loading, error] = useData('/schedule', addScheduleTimes);
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the schedule...</h1>
 
   return (
     <div className="container">
---
+++ src/utilities/firebase.js
@@ -0,0 +1,38 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase, onValue, ref } from 'firebase/database';
+import { useState, useEffect } from 'react';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDjakRU4B_tO85ucVqK9Wl5hqbBSMKSAa4",
+    authDomain: "rgoodman22-cs397-scheduler.firebaseapp.com",
+    databaseURL: "https://rgoodman22-cs397-scheduler-default-rtdb.firebaseio.com",
+    projectId: "rgoodman22-cs397-scheduler",
+    storageBucket: "rgoodman22-cs397-scheduler.appspot.com",
+    messagingSenderId: "416690190235",
+    appId: "1:416690190235:web:b4b25cc7f60b0a33b9d33e"
+};
+
+const firebase = initializeApp(firebaseConfig);
+const database = getDatabase(firebase);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+
+    useEffect(() => {
+        const dbRef = ref(database, path);
+        return onValue(dbRef, (snapshot) => {
+            const val = snapshot.val();
+            setData(transform ? transform(val) : val);
+            setLoading(false);
+            setError(null);
+        }, (error) => {
+            setData(null);
+            setLoading(false);
+            setError(error);
+        });
+    }, [path, transform]);
+
+    return [data, loading, error];
+};
